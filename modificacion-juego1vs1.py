0b571f14230f9a19f5abddfa502a41a7e163428b
joelhernandez211-id <joelhernandez2115@gmail.com>
2026-03-21 22:41:49 -0500

    modificacion final juego_1vs1.py

--- modificacion-juego1vs1.py
+++ modificacion-juego1vs1.py
@@ -1,13 +1,13 @@
-import random
+def salud(n:int):
+    mostrar_ico = ""
+    corazones="❤️"
+    n=n//10
+    for i in range(0,n,1):
+        mostrar_ico = mostrar_ico + corazones
+    return mostrar_ico
 
-
-#recursos iniciales
-
-hp_h = 100
-pocion = 3
-hp_e = 120
-opc = "0"
-while hp_h>0 and hp_e >0:
+def turno_jugador(lista,opc):
+    import random
     while opc != "1" and opc != "2" and opc != "3":
     
         opc = input("Por favor escoge una opcion:""\n 1.Atacar" \
@@ -19,9 +19,13 @@ while hp_h>0 and hp_e >0:
             
 
     #acciones dependiendo de la accion
-
+    nombre_h=lista[0]
+    hp_e=int(lista[4])
+    hp_h=int(lista[1])
+    pocion=int(lista[2])
+    accion_valida=bool(lista[5])
     if opc == "1":
-        print("==TURNO DEL HEROE==")
+        print(f"==TURNO DEL HEROE {nombre_h}==")
         daño_aleatorio = random.randint(10,25)
         print("El daño del heroe fue ",daño_aleatorio)
         hp_e = hp_e - daño_aleatorio 
@@ -29,22 +33,28 @@ while hp_h>0 and hp_e >0:
             print("Haz derrotado al enemigo")
             hp_e =0
         print("Vida del enemigo ", hp_e)
+        print(salud(hp_e))
+        lista[4]=hp_e
 
 
     elif opc == "2":
-        print("==TURNO DEL HEROE==")
+        print(f"==TURNO DEL HEROE {nombre_h}==")
         if pocion >= 1:
             hp_h = hp_h + 20
             print("Vida actual del heroe:",hp_h)
+            print(salud(hp_h))
             pocion = pocion -1
             print("Pociones restantes", pocion)
         else:
             print("No te quedan mas pociones, intenta de nuevo")
             opc = "0"
-            continue
+            accion_valida=False
+        lista[5]=accion_valida    
+        lista[1]=hp_h
+        lista[2]=pocion
 
     elif opc == "3":
-        print("==TURNO DEL HEROE==")
+        print(f"==TURNO DEL HEROE {nombre_h}==")
         habilidad_especial = random.randint(30,50)
         print("Ataque critico de: ",habilidad_especial)
         aleatorio = random.randint(1,100)
@@ -57,17 +67,29 @@ while hp_h>0 and hp_e >0:
             print("Haz derrotado al enemigo")
             hp_e =0
         print("vida de enemigo",hp_e)
-        
+        print(salud(hp_e))
+        lista[4]=hp_e
+    return lista
 
-    #ataque del enemigo 
-    if hp_e > 0:
-        print("==TURNO DEL ENEMIGO==")
-        daño_aleatorio_ene = random.randint(15,20)
-        print("El Daño del enemigo fue",daño_aleatorio_ene)
-        hp_h = hp_h - daño_aleatorio_ene
-        
+def turno_enemigo(lista):
+    import random
+    #ataque del enemigo
+    nombre_e=lista[3]
+    accion_valida=bool(lista[5])
+    hp_e=int(lista[4])
+    hp_h=int(lista[1])
+    if accion_valida==True:
+        if hp_e > 0:
+            print(f"==TURNO DEL ENEMIGO {nombre_e}==")
+            daño_aleatorio_ene = random.randint(15,20)
+            print("El Daño del enemigo fue",daño_aleatorio_ene)
+            hp_h = hp_h - daño_aleatorio_ene
+            lista[1]=hp_h
         if hp_h <=0:
             print("El enemigo te ha derrotado")
             hp_h = 0
+            lista[1]=hp_h
         print("La vida del heroe es:",hp_h)
-    opc = "0"
+        print(salud(hp_h))
+    return lista
+        
